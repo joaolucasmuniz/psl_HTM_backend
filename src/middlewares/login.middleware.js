46dc0dc4c6aa5f2476888826bc266b715c79f345
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-11-29 22:45:39 -0300

    Fix error message in login.middleware.js

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -1,7 +1,7 @@
 const validateLogin = (req, res, next) => {
   const { username } = req.body;
   if (!username || username === '') {
-    return res.status(400).json({ message: 'O campo "username" é obrigatório' }); 
+    return res.status(400).json({ error: 'O campo "username" é obrigatório' }); 
   }
   next();
 };
